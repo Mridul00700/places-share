2893c7099a545eab3121aa9797ba0b82537180d1
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-02-28 11:29:41 +0530

    list

--- frontend/src/user/components/UserList.js
+++ frontend/src/user/components/UserList.js
@@ -1,7 +1,18 @@
-
+import './UserList.css';
 
 const UserList = props => {
 
+    if (props.items.length === 0) {
+        return <div className="center">
+            <h2>No Users Found</h2>
+        </div>
+    };
+
+    return <ul>
+        {props.items.map(user => {
+            return
+        })}
+    </ul>
 }
 
 
