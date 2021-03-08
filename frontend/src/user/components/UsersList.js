225709b1be0b64811d54c81865522421be561956
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-03-09 00:47:49 +0530

    new-Comp_added

--- frontend/src/user/components/UsersList.js
+++ frontend/src/user/components/UsersList.js
@@ -1,11 +1,15 @@
-import './UsersList.css';
+
 import UserItem from './UserItem';
+import Card from '../../shared/components/UIElements/Card';
+import './UsersList.css';
 
 const UserList = props => {
 
   if (props.items.length === 0) {
     return <div className="center">
-      <h2>No Users Found</h2>
+      <Card>
+        <h2>No Users Found</h2>
+      </Card>
     </div>
   };
 
