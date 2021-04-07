977bad15ff916bbde99e0c7e31aab2f89f3243c4
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-04-07 08:40:30 +0530

    Custom Input

--- frontend/src/shared/components/FormElements/Input.js
+++ frontend/src/shared/components/FormElements/Input.js
@@ -1,10 +1,11 @@
+import "./Input.css";
 
-
-
-import './Input.css';
-
-const Input = props => {
-    return <div></div>
-}
+const Input = (props) => {
+  return (
+    <div classNames={`form-control`}>
+      <label htmlFor={props.id}>{props.label}</label>
+    </div>
+  );
+};
 
 export default Input;
