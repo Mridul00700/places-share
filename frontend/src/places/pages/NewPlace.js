4dc3e39ca611595e16082ef968bf47ce6188dedd
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-06-14 22:58:57 +0530

    inputFix

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,8 +1,12 @@
 // import Input from "";
+
+import "./NewPlace.css";
+import Input from "../../shared/components/FormElements/Input";
+
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input type="text" label="Title" />
+      <Input element="input" type="text" label="Title" />
     </form>
   );
 };
