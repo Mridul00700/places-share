706c21c08cf010d8b5155ef0ca0def6ab4a9a0a9
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-06-15 00:00:55 +0530

    useReducer_start

--- frontend/src/shared/components/FormElements/Input.js
+++ frontend/src/shared/components/FormElements/Input.js
@@ -1,11 +1,36 @@
+// import { useState } from "react";
+import { useReducer } from "react";
 import "./Input.css";
 
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.val,
+        isValid: true,
+      };
+    default:
+      return state;
+  }
+};
+
 const Input = (props) => {
+  // const [enterValue, setEntervalue] = useState("");
+  // const [isValid, setIsvalid] = useState(false);
+  useReducer(inputReducer);
+  const changeHandler = (event) => {};
+
   const element =
     props.element === "input" ? (
-      <input id={props.id} type={props.type} placeholder={props.placeholder} />
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onchange={changeHandler}
+      />
     ) : (
-      <textarea id={props.id} rows={props.rows || 3} />
+      <textarea id={props.id} rows={props.rows || 3} onchange={changeHandler} />
     );
 
   return (
