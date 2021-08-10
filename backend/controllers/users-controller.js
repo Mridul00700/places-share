113b95c34c2f62f29c525281af31721311716eff
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-08-10 20:28:16 +0530

    adding users-controller- login/signup

---
+++ backend/controllers/users-controller.js
@@ -0,0 +1,17 @@
+const getUsers = (req, res, next) => {
+
+}
+
+const signUp = (req, res, next) => {
+
+}
+
+const login = (req, res, next) => {
+
+}
+
+
+
+exports.getUsers = getUsers;
+exports.signUp = signUp;
+exports.login = login;
